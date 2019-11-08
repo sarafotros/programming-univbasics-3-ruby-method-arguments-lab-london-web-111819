d462772b7301cdceb9de22c0192e0aedacc4ce3f
Sara Fotros <sarafotros1@yahoo.com>
2019-11-08 15:49:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,10 +1,13 @@
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+# in order to co[mplete the rest of this course and be ready for the next course!
+def greeting_a_person(name)
+  puts "Hello, #{name}"
+end
 
-
-
+name = "Maria"
+greeting_a_person
 
 
 
