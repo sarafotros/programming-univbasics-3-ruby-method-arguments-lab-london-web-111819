d48244150af669680b7944c12b4d70eadeedad1d
Sara Fotros <sarafotros1@yahoo.com>
2019-11-08 15:50:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,7 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to co[mplete the rest of this course and be ready for the next course!
+
 def greeting_a_person(name)
   puts "Hello, #{name}"
 end
